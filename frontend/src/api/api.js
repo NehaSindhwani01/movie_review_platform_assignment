cb2dd41ed01bae5c330a679353c6436234a6ffe8
Neha Sindhwani <nehasindhwani01@gmail.com>
2025-09-14 00:18:58 +0530

    modified frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://movie-review-platform-assignment.onrender.com/api';
 
 const api = {
   request: async (endpoint, options = {}) => {
